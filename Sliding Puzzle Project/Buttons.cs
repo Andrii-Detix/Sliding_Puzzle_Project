0bfdc1d1f9b514828d0c2da763f0e9957756cbc5
Detix <aivancisin6@gmail.com>
2024-01-07 21:33:57 +0200

    add Buttons functional

--- Sliding Puzzle Project/Buttons.cs
+++ Sliding Puzzle Project/Buttons.cs
@@ -2,8 +2,56 @@
 
 public class Buttons
 {
-    public Buttons()
+  
+    public Buttons(int size)
+    {
+        Size = size;
+        CreateButtons();
+    }
+
+    public readonly int Size;
+    public int[,] ButtonsValue;
+    public int SelectedButtonColumn = 0;
+    public int SelectedButtonRow = 0;
+    
+    private void CreateButtons()
     {
         
+        ButtonsValue = new int[Size, Size];
+        Random random = new Random();
+        int maxValue = Size * Size;
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                while (CheckButtonRepeat(i, j))
+                {
+                    ButtonsValue[i, j] = random.Next(0, maxValue);
+                }
+            }
+        }
     }
+
+    private bool CheckButtonRepeat(int currentButtonRow, int currentButtonColumn)
+    {
+        bool isRepeat = false;
+        for (int i = 0; i <= currentButtonRow; i++)
+        {
+            int iterationsLimit = (i == currentButtonRow ? currentButtonColumn : Size);
+            
+            for (int j = 0; j < iterationsLimit; j++)
+            {
+                if (ButtonsValue[i,j] == ButtonsValue[currentButtonRow,currentButtonColumn])
+                {
+                    isRepeat = true;
+                    break;
+                }
+            }
+            if (isRepeat)
+                break;
+        }
+
+        return isRepeat;
+    }
+    
 }
